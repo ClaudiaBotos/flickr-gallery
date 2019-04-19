bbf679839abbe8c8ff8fcc01d11925050e2a3746
Claudia Botos <claudia.nicoleta94@gmail.com>
2019-04-19 09:13:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import ErrorPage from './Components/ErrorPage';
 import NotFound from './Components/MainContainer/NotFound';
 
 const App =(props) => (
-  <BrowserRouter>
+  <BrowserRouter basename={/index.html}>
   <div className="container">
  <Switch>
 
